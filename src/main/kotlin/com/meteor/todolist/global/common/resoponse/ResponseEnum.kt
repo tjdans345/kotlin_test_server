e9dd87732cb2d17104b826dc7aaab42da811e1cb
meteor <tjdans34568@gmail.com>
2022-05-17 16:30:17 +0900

    임시 수정파일

--- src/main/kotlin/com/meteor/todolist/global/common/resoponse/ResponseEnum.kt
+++ src/main/kotlin/com/meteor/todolist/global/common/resoponse/ResponseEnum.kt
@@ -3,8 +3,9 @@ package com.meteor.todolist.global.common.resoponse
 enum class ResponseEnum(val code: Int, val message: String) {
 
     EXAMPLE_ENUM(1, "예시입니다."),
-    EXAMPLE_ENUM2(2, "예시2입니다.");
+    EXAMPLE_ENUM2(2, "예시2입니다."),
+    EXAMPLE_ENUM3(3, "예시3입니다."),
 
 //    fun getCode() = this.code
-//    fun getEnumMessage() =  this.enumMessage
+//    fun getEnumMessage() =  this.enumMessßßßage
 }
