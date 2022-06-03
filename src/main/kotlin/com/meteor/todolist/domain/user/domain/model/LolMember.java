2a6b2d37be0e6a05538aa2ca202c24f17bc3ae63
meteor <tjdans34568@gmail.com>
2022-06-03 15:26:52 +0900

    임시 수정파일

---
+++ src/main/kotlin/com/meteor/todolist/domain/user/domain/model/LolMember.java
@@ -0,0 +1,47 @@
+package com.meteor.todolist.domain.user.domain.model;
+
+// json 번역용
+public class LolMember {
+
+    // json
+    private String assis;
+    private String sommonId;
+    private int kill;
+
+}
+
+// json 번역용 (날씨 API)
+class WheaterDataModel {
+
+
+    private String assis;
+    private String sommonId;
+    private int kill;
+
+}
+
+// UI에 반환 할 데이터
+class resultModel {
+    LolMember lolmember;
+    WheaterDataModel wheaterDataModel;
+}
+
+class 너한테필요한것 {
+
+    // 1. 기준 유저(검색한 유저)
+    MatachModel serachUser;
+
+    // 2. 그외 쭉정이
+    List<MatchModel> allList;
+
+}
+
+
+// java <-> flutter
+// java <->  C, C# , JS, TS, Ko
+// json <<< 영어
+// java -> json(영어) <- flutter, js, ko  ->
+
+// 1. 기준이 되는 유저의 정보 데이터
+// 2. 그 외 쭉정이 유저들 리스트
+
